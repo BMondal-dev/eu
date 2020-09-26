0576b834f25fd38158ed9846c5fb99535e43624b
BMondal-dev <71638574+BMondal-dev@users.noreply.github.com>
2020-09-26 12:14:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var http = require('http);
-http.createServer(function(req,res){
-  res.writeHead(200,{'Content-Type':'text/html'});
-  res.end('Hello By Heroku');
-}).listen(process.env.PORT);
+const http = require('http'); //loads the library to enable it to act as a server
+var port = process.env.PORT || 5000; //specifies the port no to whatever heroku gives or 5000 on local host
+http.createServer(function(req,res){ // creates a server
+    res.writeHead(200,{'Content-type':'text/plain'}); //Specifies that the respones "hello" is a text
+    res.end("hello"); //shows the text "hello" on th eweb page
+}).listen(port); // attaches this server to the port no.
