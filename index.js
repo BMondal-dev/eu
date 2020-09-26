fff0fe776ba968de42d1a1f52e19384010f350c3
BMondal-dev <71638574+BMondal-dev@users.noreply.github.com>
2020-09-26 12:00:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-var https = require('https');
-var formidable = require('formidable');
-var fs = require('fs');
-
-https.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'uploads/' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
-}).listen(8080);
-
+var http = require('http);
+http.createServer(function(req,res){
+  res.writeHead(200,{'Content-Type':'text/html'});
+  res.end('Hello By Heroku');
+}).listen(3000);
